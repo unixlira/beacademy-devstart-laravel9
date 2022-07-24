04503a670328c377878a77a2286cfd62967441fe
José Lira <joserobertolira@gmail.com>
2022-07-24 01:03:18 -0300

    public images

--- resources/views/template/admin.blade.php
+++ resources/views/template/admin.blade.php
@@ -9,7 +9,7 @@
 </head>
 <body class="bg-gray-50">
     <div class="container mx-auto px-60 py-8">
-        <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
+        <nav class="bg-gray-100 border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
             <div class="container flex flex-wrap justify-between items-center mx-auto">
                 <a href="/" class="flex items-center">
                     <img src="{{url('/logo/logo-ebook.png')}}" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
